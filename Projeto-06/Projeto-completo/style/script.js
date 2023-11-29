38da112045e0a491d99186502e6622e6218a383d
Ferreira-well <w.ferreira.lopes04@gmail.com>
2023-11-29 15:38:10 -0300

    Add files via upload

---
+++ Projeto-06/Projeto-completo/style/script.js
@@ -0,0 +1,22 @@
+const wrapper= document.querySelector('.wrapper');
+const loginlink=document.querySelector('.login-link');
+const registerlink= document.querySelector('.register-link');
+const btnpoup= document.querySelector('.btnLogin-popup');
+const iconclose= document.querySelector('.icon-close');
+
+
+registerlink.addEventListener('click',()=>{
+    wrapper.classList.add('active')
+});
+
+loginlink.addEventListener('click',()=>{
+    wrapper.classList.add('active')
+});
+
+btnpoup.addEventListener('click',()=>{
+    wrapper.classList.add('active-poup')
+});
+
+iconclose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-poup')
+});
